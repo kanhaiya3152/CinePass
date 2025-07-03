6f4414ce629408808401546095928a770a6a7582
Kanhaiya Mandal <kanhaiya3152@gmail.com>
2025-07-04 00:22:50 +0530

    feat: implement routing and layout structure in App component

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,11 +1,32 @@
 import React from "react"
+import Navbar from "./components/Navbar"
+import { Route, Routes, useLocation } from "react-router-dom"
+import Home from "./pages/Home"
+import Movie from "./pages/Movie"
+import MovieDetails from "./pages/MovieDetails"
+import SeatLayout from "./pages/SeatLayout"
+import MyBookings from "./pages/MyBookings"
+import Favourite from "./pages/Favourite"
+import Toaster from 'react-hot-toast'
+import Footer from "./components/Footer"
 
 const App = () => {
 
+  const isAdminRoute = useLocation().pathname.startsWith('/admin')
   return (
-    <div>
-      
-    </div>
+    <>
+    <Toaster/>
+    {!isAdminRoute && <Navbar/>}
+    <Routes>
+      <Route path="/" element ={<Home/>}/>
+      <Route path="/movies" element ={<Movie/>}/>
+      <Route path="/movies/:id" element ={<MovieDetails/>}/>
+      <Route path="/movies/:id/:date" element ={<SeatLayout/>}/>
+      <Route path="/my-bookings" element ={<MyBookings/>}/>
+      <Route path="/favourite" element ={<Favourite/>}/>
+    </Routes>
+    {!isAdminRoute && <Footer/>}
+    </>
   )
 }
 
