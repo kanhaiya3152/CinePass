2d3a6dd2e0f47bd9ecfffadfbe868680f91c2333
Kanhaiya Mandal <kanhaiya3152@gmail.com>
2025-07-06 22:37:45 +0530

    feat: refactor HeroSection component to include movie details and icons

--- client/src/components/HeroSection.jsx
+++ client/src/components/HeroSection.jsx
@@ -1,15 +1,26 @@
 import React from "react"
 import { assets } from "../assets/assets"
-
-const Footer = () => {
+import {CalendarIcon, ClockIcon} from 'lucide-react'
+ 
+const HeroSection = () => {
 
     return (
         <div className='flex flex-col items-start justify-center gap-4 px-6 md:px-16 1g:px-36 bg-[url("/backgroundImage.png")] bg-cover bg-center h-screen'>
             <img src={assets.marvelLogo} alt="" className="max-h-11 lg:h-11 mt-20" />
             <h1 className='text-5x1 md:text-[70px] md:leading-18 font-semibold
-max-w-110'>Guardians <br /> of the Galaxy</h1>
+            max-w-110'>Guardians <br /> of the Galaxy</h1>
+
+            <div className="flex items-center gap-4 text-gray-300">
+                <span>Action | Adventure | Sci-Fi</span>
+                <div className="flex items-center gap-1">
+                    <CalendarIcon className='w-4.5 h-4.5'/> 2018
+                </div>
+                <div className="flex items-center gap-1">
+                    <ClockIcon className='w-4.5 h-4.5'/> 2h 8m
+                </div>
+            </div>
         </div>
     )
 }
 
-export default Footer
+export default HeroSection
