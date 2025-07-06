bf819a01ae211ee1e504c4fdda0eed5a469d901a
Kanhaiya Mandal <kanhaiya3152@gmail.com>
2025-07-06 22:37:56 +0530

    feat: update Home component to render HeroSection

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,11 +1,12 @@
 import React from "react"
+import HeroSection from "../components/HeroSection"
 
 const Home = () => {
 
   return (
-    <div>
-      
-    </div>
+    <>
+      <HeroSection/>
+    </>
   )
 }
 
